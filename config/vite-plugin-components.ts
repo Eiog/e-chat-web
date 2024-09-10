a0063624b6d05f01603ca65d8f001de53425765e
Eiog <33677659+Eiog@users.noreply.github.com>
2024-09-10 15:33:45 +0800

    Initial commit

---
+++ config/vite-plugin-components.ts
@@ -0,0 +1,23 @@
+import {
+  NaiveUiResolver,
+  VueUseComponentsResolver,
+  VueUseDirectiveResolver,
+} from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+import type { PluginOption } from 'vite'
+
+export function VitePluginComponents(): PluginOption[] {
+  return [
+    Components({
+      dirs: ['src/components', 'src/layouts'],
+      extensions: ['vue', 'md'],
+      deep: true,
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      resolvers: [
+        NaiveUiResolver(),
+        VueUseComponentsResolver(),
+        VueUseDirectiveResolver(),
+      ],
+    }), // https://github.com/antfu/unplugin-vue-components
+  ]
+}
---
+++ src/composables/useNProgress.ts
@@ -0,0 +1,14 @@
+/* eslint-disable unicorn/consistent-function-scoping */
+import NProgress from 'nprogress'
+import '~/assets/style/nprogress.css'
+
+export function useNProgress() {
+  function start() {
+    if (!NProgress.isStarted())
+      NProgress.start()
+  }
+  function done() {
+    NProgress.done()
+  }
+  return { start, done }
+}
